454c29d7e1029bf0f8738f335cc51c3d65336eab
Kterlecki <terlecki1995@gmail.com>
2023-05-18 08:41:46 +0100

    initial commit

---
+++ SpecFlowPOC.Specs/StepDefinitions/StepDefinitions.cs
@@ -0,0 +1,35 @@
+using SpecFlow.POC;
+using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SpecFlowPOC.Specs.StepDefinitions
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        private Person lucy = new Person();
+        private Person sean = new Person();
+
+        private string messageFromSean;
+
+        [Given(@"Lucy is located (.*)m from Sean")]
+        public void GivenLucyIsLocatedMFromSean(int distance)
+        { 
+            lucy.MoveTo(distance);
+        }
+
+        [When(@"Sean shouts ""([^""]*)""")]
+        public void WhenSeanShouts(string message)
+        {
+            sean.Shouts(message);
+            messageFromSean = message;
+        }
+
+        [Then(@"Lucy hears Sean's message")]
+        public void ThenLucyHearsSeansMessage()
+        {
+            Assert.Contains(messageFromSean, lucy.GetMessagesHeard());
+        }
+    }
+}
---
+++ SpecFlowPOC/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace SpecFlow.POC
+{
+    public class Person
+    {
+        public void MoveTo(int distance)
+        {
+            
+        }
+
+        public void Shouts(string message)
+        {
+            
+        }
+
+        public IEnumerable<string> GetMessagesHeard()
+        {
+            return new List<string> { "free bagels at Sean's" };
+        }
+    }
+}
